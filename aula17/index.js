7e97cc3eac6a0a7c7f34b500a362739e1dd7fcb8
Azinth <gsv.jesus@gmail.com>
2022-06-17 22:05:07 -0300

    aula17

---
+++ aula17/index.js
@@ -0,0 +1,11 @@
+ const alunos = ['Gabriel', 'Pedro', 'João', 'Maria', 'José'];
+
+ 
+ console.log(alunos)
+ 
+ alunos.pop(); //remove o último elemento do array e pode salvar o array em uma variável
+ alunos.shift(); //remove o primeiro elemento do array e pode salvar o array em uma variável
+ alunos.unshift('Josias'); //adiciona no inicio do array
+ alunos.push('Souza') //adiciona um novo elemento no final do array
+
+ //se eu quiser deletar um indice de um array eu uso o delete "nome da variável" [indice].
