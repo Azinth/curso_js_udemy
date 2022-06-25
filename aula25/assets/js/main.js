2e9a0a31da95e188fc8b542f85af9e935877a106
Azinth <gsv.jesus@gmail.com>
2022-06-25 00:15:35 -0300

    desafio_concluido

--- aula25/assets/js/main.js
+++ aula25/assets/js/main.js
@@ -1,50 +1,32 @@
-function meuEscopo () {
+const form = document.querySelector('.form')
+const container = document.querySelector('.container')
+const resultado = document.querySelector('.resultado')
 
-  const form = document.querySelector('.form');
-  const resultado = document.querySelector('.resultado');
+function recebeEventoForm(evento) {
+  evento.preventDefault()
+  const peso = form.querySelector('.peso')
+  const pesoDigitado = parseFloat(peso.value)
+  const altura = form.querySelector('.altura')
+  const alturaDigitada = parseFloat(altura.value)
+  const imc = (pesoDigitado / (alturaDigitada * alturaDigitada)).toFixed(1)
 
-  function recebeEventoForm (evento) {
-    evento.preventDefault();
-    const peso = form.querySelector('.peso');
-    const pesoDigitado = parseFloat(peso.value);
-    const altura = form.querySelector('.altura');
-    const alturaDigitada = parseFloat(altura.value);
+  if (pesoDigitado === 0 || alturaDigitada === 0 || isNaN(imc) || imc === Infinity || imc === -Infinity || imc === 'NaN' || pesoDigitado === 'NaN' || alturaDigitada === 'NaN' || pesoDigitado === Infinity || alturaDigitada === Infinity || pesoDigitado === -Infinity || alturaDigitada === -Infinity || pesoDigitado === null || alturaDigitada === null) {
+    alert('Preencha todos os campos')
 
-    
-    // if (alturaDigitada != undefined && alturaDigitada != null && alturaDigitada != '' && alturaDigitada != NaN){
-    //   const imc = pesoDigitado / (alturaDigitada * alturaDigitada);
-    //   // resultado.innerHTML = `<p>IMC: ${imc}<br/></p>`;
-    //   alert(`IMC: ${imc}`);
-    // } else {
-      
-    // }
-
-    if (alturaDigitada != undefined && alturaDigitada != null && alturaDigitada != '' && alturaDigitada != NaN){
-      if (pesoDigitado != undefined && pesoDigitado != null && pesoDigitado != '' && pesoDigitado != NaN){
-        const imc = pesoDigitado / (alturaDigitada * alturaDigitada);
-        resultado.innerHTML += `<p>IMC: ${imc}<br/></p>`;
-      } else {
-        alert('Peso inválido');
-      }
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
+  } else if (imc < 10 ) {
+    alert('Preencha os campos corretamente')
+  } else if (imc <= 18.5) {
+    resultado.innerHTML = `<div class="resultadoVermelho"><p>Seu IMC é ${imc}: Abaixo do peso</p></div>`
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    resultado.innerHTML = `<div class="resultadoVerde"><p>Seu IMC é ${imc}: Peso ideal</p></div>`
+  } else if (imc >= 25 && imc <= 29.9) {
+    resultado.innerHTML = `<div class="resultadoAmarelo"><p>Seu IMC é ${imc}: Sobrepeso</p></div>`
+  } else if (imc >= 30 && imc <= 34.9) {
+    resultado.innerHTML = `<div class="resultadoVermelho"><p>Seu IMC é ${imc}: Obesidade grau I</p></div>`
+  } else if (imc >= 35 && imc <= 39.9) {
+    resultado.innerHTML = `<div class="resultadoVermelho"><p>Seu IMC é ${imc}: Obesidade grau II</p></div>`
+  } else if (imc >= 40) {
+    resultado.innerHTML = `<div class="resultadoVermelho"><p>Seu IMC é ${imc}: Obesidade grau III</p></div>`
   }
-  form.addEventListener('submit', recebeEventoForm);
-
 }
-
-meuEscopo();
+form.addEventListener('submit', recebeEventoForm)
