9802e9370cc05d13d28c51551d41258ab6bd5462
Azinth <gsv.jesus@gmail.com>
2022-06-27 17:25:09 -0300

    aula27

---
+++ aula27/opecacaoTernaria.js
@@ -0,0 +1,22 @@
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >=1000 ? 'Usuário Vip' : 'Usuário Normal';
+console.log(nivelUsuario)
+/*
+Um if pode ser encurtado da seguinte forma:
+
+
+if (pontuacaoUsuario >= 1000) {
+  console.log("Usuário VIP");
+} else {
+  console.log("Usuário Normal");
+}
+
+encurtando:
+const nivelUsuario = pontuacaoUsuario >=1000 ? 'Usuário Vip' : 'Usuário Normal';
+
+(condicao) ? true : false;
+
+*/
+
+
+
