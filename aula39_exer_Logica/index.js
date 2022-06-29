90b5ffe497e5b2042904ceb93db53990aa6f8797
Azinth <gsv.jesus@gmail.com>
2022-06-29 19:01:42 -0300

    aula39

---
+++ aula39_exer_Logica/index.js
@@ -0,0 +1,11 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function maior(x, y) {
+  // if (num1 > num2)return num1;
+  // return num2;
+  return x>y?x:y;
+}
+
+const max2 = (x, y) => x>y?x:y;
+
+console.log(max2(10, 50)); // 10
